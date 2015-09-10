eafd9113b02f81964735e58ce543c3bbaaa4c133
Frederic Hamel <frederic.hamel123@gmail.com>
2015-09-10 12:03:19 -0400

    Prepare le renommage de la library
    GameGraphics -> UGEN

---
+++ cmake/AddUGENLibrary.cmake
@@ -0,0 +1,19 @@
+
+file(GLOB src "UGEN/src/*.cpp" "UGEN/src/subsystem/*.cpp" "UGEN/src/exceptions/*.cpp")
+
+include_directories("UGEN/include")
+
+add_library(UGEN SHARED ${src})
+target_link_libraries(GameGraphics
+	${SDL2_LIBRARY}
+	${SDL2_IMAGE_LIBRARY}
+	${SDL2_TTF_LIBRARY}
+	${OPENGL_gl_LIBRARY}
+	${PHYSFS_LIBRARY})
+
+install(DIRECTORY "UGEN/include" DESTINATION ".")
+
+install(TARGETS UGEN
+	RUNTIME DESTINATION bin
+	LIBRARY DESTINATION lib
+	ARCHIVE DESTINATION lib/static)
