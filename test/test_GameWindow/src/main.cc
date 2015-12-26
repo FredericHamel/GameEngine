4b1f1d96c6c42d51b9b04c549b3e8fcd954c1738
Frederic Hamel <frederic.hamel123@gmail.com>
2015-12-26 00:18:15 -0500

    Update unit testing of GameWindow

--- test/test_GameWindow/src/main.cc
+++ test/test_GameWindow/src/main.cc
@@ -16,16 +16,23 @@ int main()
 		Video video;
 		GameWindow win("Window", 0, 0, 640, 480, SDL_WINDOW_SHOWN|SDL_WINDOW_OPENGL);
 		std::cin.sync_with_stdio(false);
+		std::cout << "Press Ctrl-D to exit...\n";
+		while(1) {
+			glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			win.updateDraw();
+			if(read() == EOF) break;
 
-		glClearColor(0.0f, 0.0f, 0.4f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		win.updateDraw();
-		read();
-
-		glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		win.updateDraw();
-		read();
+			glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			win.updateDraw();
+			if(read() == EOF) break;
+			
+			glClearColor(0.6f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+			win.updateDraw();
+			if(read() == EOF) break;
+		}
 	}catch(std::exception& e)
 	{
 		std::cout << e.what() << std::endl;
