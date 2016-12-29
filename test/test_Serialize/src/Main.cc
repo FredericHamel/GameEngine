12047d66a3018e9196e9f72bcb386c614eb7edfa
Frederic Hamel <frederic.hamel123@gmail.com>
2016-12-29 14:31:36 -0500

    Improve test of ugen::Serializable\n

--- test/test_Serialize/src/Main.cc
+++ test/test_Serialize/src/Main.cc
@@ -5,31 +5,33 @@
 #include <cstdarg>
 
 void log(int argc, const char *type, ...) {
-	va_list va;
-	va_start(va, type);
-	for(int i = 0; i < argc; ++i) {
-		switch(type[i]) {
-			case 'P':
-				std::cout << "Point: " << va_arg(va, ugen::Point2D) << std::endl;
-				break;
-			case 'R':
-				std::cout << va_arg(va, ugen::Rectangle) << std::endl;
-		}
-	}
-	va_end(va);
+  va_list va;
+  va_start(va, type);
+  for(int i = 0; i < argc; ++i) {
+    switch(type[i]) {
+      case 'P':
+        std::cout << "Point: " << va_arg(va, ugen::Point2D) << std::endl;
+        break;
+      case 'R':
+        std::cout << va_arg(va, ugen::Rectangle) << std::endl;
+    }
+  }
+  va_end(va);
 }
 int main(int argc, char **argv) {
-	ugen::Point2D dot(45, 60);
-	ugen::Rectangle rect(dot, 640, 480);
-	
-	log(2, "PR", dot, rect);
-	
-	dot.Move(-10, 5);
-	
-	log(2, "PR", dot, rect);
-	
-	rect.Move(10, -5);
+  ugen::Point2D dot(45, 60);
+  ugen::Rectangle rect(dot, 640, 480);
+ 
+  std::cout << "Expected dot (45, 60). Got " << dot << std::endl;
+  std::cout << "Expected rect [45, 60, 640, 480]. Got " << rect << std::endl;
+  std::cout << "Move dot -> (-10, 5)\n";
+  dot.Move(-10, 5);
 
-	log(2, "PR", dot, rect);
-	return 0;
+  log(2, "PR", dot, rect);
+
+  std::cout << "Move rect -> (10, -5)\n";
+  rect.Move(10, -5);
+
+  log(2, "PR", dot, rect);
+  return 0;
 }
